d706f526ea7805cdd2df3ffb55e784b88ffd4436
Ulfath Ara Islam <mouliajsahid@gmail.com>
2023-05-31 06:35:02 +0900

    displayed all toys data in a table

--- src/pages/Toys/Toys.jsx
+++ src/pages/Toys/Toys.jsx
@@ -1,16 +1,16 @@
 import React, { useEffect, useState } from 'react';
 
 const Toys = () => {
-    const [toys, setToys] = useState([]);
+  const [toys, setToys] = useState([]);
 
-    useEffect(() => {
-        fetch('http://localhost:5000/toys')
-            .then((res) => res.json())
-            .then((data) => setToys(data));
-    }, []);
+  useEffect(() => {
+    fetch('http://localhost:5000/toys')
+      .then((res) => res.json())
+      .then((data) => setToys(data));
+  }, []);
 
-    return (
-        <div className="container mx-auto px-4 py-8">
+  return (
+    <div className="container mx-auto px-4 py-8">
       <h3 className="text-4xl text-center">All Toys: {toys.length}</h3>
 
       <table className="mt-8 w-full">
@@ -25,11 +25,20 @@ const Toys = () => {
           </tr>
         </thead>
         <tbody>
-          
+          {toys.map((toy) => (
+            <tr key={toy.id}>
+              <td className="py-2 px-4 border-b">{toy.seller.name}</td>
+              <td className="py-2 px-4 border-b">{toy.toy_name}</td>
+              <td className="py-2 px-4 border-b">{toy.category}</td>
+              <td className="py-2 px-4 border-b">{toy.price}</td>
+              <td className="py-2 px-4 border-b">{toy.available_quantity}</td>
+              <td className="py-2 px-4 border-b"></td>
+            </tr>
+          ))}
         </tbody>
       </table>
     </div>
-    );
+  );
 };
 
 export default Toys;
