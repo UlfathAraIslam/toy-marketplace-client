3170bb49179459ef882cb414a1da0cf55f3d3bfa
Ulfath Ara Islam <mouliajsahid@gmail.com>
2023-06-01 16:57:13 +0900

    toys are adding from client to database

--- src/pages/AddAToy/AddAToy.jsx
+++ src/pages/AddAToy/AddAToy.jsx
@@ -1,103 +1,142 @@
-import React, { useContext} from 'react';
-import { useLoaderData } from 'react-router-dom';
+import React, { useContext } from 'react';
 import { AuthContext } from '../../providers/AuthProvider';
+import Swal from 'sweetalert2';
+
 
 const AddAToy = () => {
-
-
-  const {user} = useContext(AuthContext);
+  const { user } = useContext(AuthContext);
 
   const handleAddToy = event => {
     event.preventDefault();
 
     const form = event.target;
-    const name =form.name.value;
+    const name = form.name.value;
     const email = user?.email;
+    const available_quantity = form.available_quantity.value;
+    const category = form.category.value;
+    const rating = form.rating.value;
+    const toy_name = form.toy_name.value;
+    const price = form.price.value;
+    const picture = form.picture.value;
+
     const adding = {
       sellerName: name,
       email,
-      // // toy: _id,
-      // price: price
-    }
+      available_quantity,
+      category,
+      rating,
+      toy_name,
+      price,
+      picture
+    };
+
     console.log(adding);
-  }
 
-  // send data to server
-
-  fetch('http://localhost:5000/toys/addedToy', {
-    method: 'POST',
-    headers: {
-      'content-type': 'application/json'
-    },
-    body: JSON.stringify()
-  })
-  .then(res => res.json())
-  .then(data => {
-    console.log(data);
-    // if(data.insertedId){
-    //   alert('added toy successfully')
-    // }
-  })
+    fetch('http://localhost:5000/toys/addedToy', {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json'
+      },
+      body: JSON.stringify(adding)
+    })
+      .then(res => res.json())
+      .then(data => {
+        console.log(data);
+        if (data.insertedId) {
+          // Swal.fire({
+          //   title: 'Success',
+          //   text: 'Toy added successfully!',
+          //   icon: 'success',
+          //   confirmButtonColor: '#3085d6',
+          //   confirmButtonText: 'OK'
+          // });
+        }
+      });
+  };
 
   return (
     <div>
-      <h2>Add a toy</h2>
-            <form onSubmit={handleAddToy}>
-            <div className="card-body">
-              <div className="form-control">
-                <label className="label">
-                  <span className="label-text">Name</span>
-                </label>
-                <input type="text" name="name" className="input input-bordered" />
-              </div>
-              <div className="form-control">
-                <label className="label">
-                  <span className="label-text">Seller Name</span>
-                </label>
-                <input type="text" name="sellerName" defaultValue={user?.displayName}  className="input input-bordered" />
-              </div>
-              <div className="form-control">
-                <label className="label">
-                  <span className="label-text">Seller Email</span>
-                </label>
-                <input type="text" name="sellerEmail" placeholder='email' defaultValue={user?.email} className="input input-bordered" />
-              </div>
-              <div className="form-control">
-                <label className="label">
-                  <span className="label-text">Sub-category</span>
-                </label>
-                <input type="text" name="subCategory" className="input input-bordered" />
-              </div>
-              <div className="form-control">
-                <label className="label">
-                  <span className="label-text">Price</span>
-                </label>
-                <input type="text" name="price" className="input input-bordered" />
-              </div>
-              <div className="form-control">
-                <label className="label">
-                  <span className="label-text">Rating</span>
-                </label>
-                <input type="text" name="rating" className="input input-bordered" />
-              </div>
-              <div className="form-control">
-                <label className="label">
-                  <span className="label-text">Available quantity</span>
-                </label>
-                <input type="text" name="availableQuantity" className="input input-bordered" />
-              </div>
-              <div className="form-control">
-                <label className="label">
-                  <span className="label-text">Detail description</span>
-                </label>
-                <input type="text" name="detailDescription" className="input input-bordered" />
-              </div>
-              <div className="form-control mt-6">
-                <input className='btn bg-purple-300 text-white font-bold py-2 px-4 rounded' type="submit" value='Add Toy' />
-              </div>
-            </div>
-            </form>
+      <h2 className="text-2xl font-bold mb-4 text-center">Add a toy</h2>
+
+      <form onSubmit={handleAddToy}>
+        <div className="card-body">
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Seller Name</span>
+            </label>
+            <input
+              type="text"
+              name="sellerName"
+              defaultValue={user?.displayName}
+              className="input input-bordered"
+            />
           </div>
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Seller Email</span>
+            </label>
+            <input
+              type="text"
+              name="sellerEmail"
+              placeholder="email"
+              defaultValue={user?.email}
+              className="input input-bordered"
+            />
+          </div>
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Toy Name</span>
+            </label>
+            <input type="text" name="toy_name" className="input input-bordered" />
+          </div>
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Toy Picture</span>
+            </label>
+            <input type="text" name="picture" className="input input-bordered" />
+          </div>
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Sub-category</span>
+            </label>
+            <input type="text" name="category" className="input input-bordered" />
+          </div>
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Price</span>
+            </label>
+            <input type="text" name="price" className="input input-bordered" />
+          </div>
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Rating</span>
+            </label>
+            <input type="text" name="rating" className="input input-bordered" />
+          </div>
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Available quantity</span>
+            </label>
+            <input type="text" name="available_quantity" className="input input-bordered" />
+          </div>
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Detail description</span>
+            </label>
+            <input type="text" name="detailDescription" className="input input-bordered" />
+          </div>
+
+
+          <div className="form-control mt-6">
+            <input
+              className="btn bg-purple-300 text-white font-bold py-2 px-4 rounded"
+              type="submit"
+              value="Add Toy"
+            />
+          </div>
+        </div>
+      </form>
+    </div>
   );
 };
 
--- src/pages/MyToy/MyToy.jsx
+++ src/pages/MyToy/MyToy.jsx
@@ -1,19 +1,19 @@
-import React, { useEffect } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { AuthContext } from '../../providers/AuthProvider';
 
 const MyToy = () => {
     const {user} = useContext(AuthContext);
     const [added, setAdded] = useState([]);
 
-    const url = `http://localhost:5000/addedToy?email=${user.email}`;
+    const url = `http://localhost:5000/addedToy?email=${user?.email}`;
     useEffect(()=> {
         fetch(url)
         .then(res => res.json())
-        .then(data => console.log(data))
+        .then(data => setAdded(data))
     }, [])
     return (
         <div>
-            <h2>My Toy page</h2>
+            <h2>My Toy : {added.length}</h2>
         </div>
     );
 };
